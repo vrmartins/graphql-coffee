0f4fb8857d0bd0f1f6ea610946aff5f898a5a6ad
Vilmar Martins <vilmar.rodrigs@gmail.com>
2022-12-15 20:45:39 -0300

    add typeorm

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,13 +1,19 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType({ description: 'Coffee model' })
 export class Coffee {
+  @PrimaryGeneratedColumn()
   @Field(() => ID, { description: 'A unique identifier' })
   id: number;
 
+  @Column()
   name: string;
 
+  @Column()
   brand: string;
 
+  @Column({ type: 'json' })
   flavors: string[];
 }
