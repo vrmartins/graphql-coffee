a6f66f32fa4f0596f50c88f00f06cfe9a6204134
Vilmar Martins <vilmar.rodrigs@gmail.com>
2022-12-15 21:12:51 -0300

    add input validation

--- src/coffees/dto/create-coffee.input/create-coffee.input.ts
+++ src/coffees/dto/create-coffee.input/create-coffee.input.ts
@@ -1,7 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { MinLength } from 'class-validator';
 
 @InputType()
 export class CreateCoffeeInput {
+  @MinLength(3)
   @Field({ description: 'Coffee name' })
   name: string;
   brand: string;
