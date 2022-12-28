2f790b74775ff1daaac1a57293983694380eb841
Vilmar Martins <vilmar.rodrigs@gmail.com>
2022-12-28 11:51:38 -0300

    add Field Middleware

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,7 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { CoffeeType } from 'src/common/enums/coffee-type.enum';
 import { Drink } from 'src/common/interfaces/drink.interface';
+import { loggerMiddleware } from 'src/common/middleware/logger.middleware';
 import {
   Column,
   CreateDateColumn,
@@ -18,6 +19,7 @@ export class Coffee {
   @Field(() => ID, { description: 'A unique identifier' })
   id: number;
 
+  @Field({ middleware: [loggerMiddleware] })
   @Column()
   name: string;
 
